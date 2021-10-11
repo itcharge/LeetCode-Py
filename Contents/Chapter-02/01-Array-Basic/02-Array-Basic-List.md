e40cc54c95e20ef72d9ef84ad8ca2cffeca58d5a
ITCharge <i@itcharge.cn>
2021-10-11 13:39:08 +0800

    更新自动生成分类题解脚本

---
+++ Contents/Chapter-02/01-Array-Basic/02-Array-Basic-List.md
@@ -0,0 +1,23 @@
+### 数组基础题目
+
+#### 数组操作题目
+
+| 题号 | 标题 | 题解 | 标签 | 难度 |
+| :------ | :------ | :------ | :------ | :------ |
+| 0189 | [旋转数组](https://leetcode-cn.com/problems/rotate-array/) | [Python](https://github.com/itcharge/LeetCode-Py/blob/main/Solutions/0189.%20%E6%97%8B%E8%BD%AC%E6%95%B0%E7%BB%84.md) | 数组 | 中等 |
+| 0066 | [加一](https://leetcode-cn.com/problems/plus-one/) | [Python](https://github.com/itcharge/LeetCode-Py/blob/main/Solutions/0066.%20%E5%8A%A0%E4%B8%80.md) | 数组 | 简单 |
+| 0724 | [寻找数组的中心下标](https://leetcode-cn.com/problems/find-pivot-index/) | [Python](https://github.com/itcharge/LeetCode-Py/blob/main/Solutions/0724.%20%E5%AF%BB%E6%89%BE%E6%95%B0%E7%BB%84%E7%9A%84%E4%B8%AD%E5%BF%83%E4%B8%8B%E6%A0%87.md) | 数组 | 简单 |
+| 0485 | [最大连续 1 的个数](https://leetcode-cn.com/problems/max-consecutive-ones/) | [Python](https://github.com/itcharge/LeetCode-Py/blob/main/Solutions/0485.%20%E6%9C%80%E5%A4%A7%E8%BF%9E%E7%BB%AD%201%20%E7%9A%84%E4%B8%AA%E6%95%B0.md) | 数组 | 简单 |
+
+#### 二维数组题目
+
+| 题号 | 标题 | 题解 | 标签 | 难度 |
+| :------ | :------ | :------ | :------ | :------ |
+| 面试题 01.07 | 旋转矩阵 |   |   |   |
+| 面试题 01.08 | 零矩阵 |   |   |   |
+| 0498 | 对角线遍历 |   |   |   |
+| 0048 | [旋转图像](https://leetcode-cn.com/problems/rotate-image/) | [Python](https://github.com/itcharge/LeetCode-Py/blob/main/Solutions/0048.%20%E6%97%8B%E8%BD%AC%E5%9B%BE%E5%83%8F.md) | 数组 | 中等 |
+| 0118 | [杨辉三角](https://leetcode-cn.com/problems/pascals-triangle/) | [Python](https://github.com/itcharge/LeetCode-Py/blob/main/Solutions/0118.%20%E6%9D%A8%E8%BE%89%E4%B8%89%E8%A7%92.md) | 数组 | 简单 |
+| 0119 | [杨辉三角 II](https://leetcode-cn.com/problems/pascals-triangle-ii/) | [Python](https://github.com/itcharge/LeetCode-Py/blob/main/Solutions/0119.%20%E6%9D%A8%E8%BE%89%E4%B8%89%E8%A7%92%20II.md) | 数组 | 简单 |
+| 0073 | [矩阵置零](https://leetcode-cn.com/problems/set-matrix-zeroes/) | [Python](https://github.com/itcharge/LeetCode-Py/blob/main/Solutions/0073.%20%E7%9F%A9%E9%98%B5%E7%BD%AE%E9%9B%B6.md) | 数组 | 中等 |
+
--- Script/create_categories_list.py
+++ Script/create_categories_list.py
@@ -28,7 +28,7 @@ def gen_markdown_table(frame):
 	lines += [line]
 	
 	## 数据部分
-	frame = frame.sort_values(by='题号')
+#	frame = frame.sort_values(by='题号')
 	frame = frame.reset_index(drop=True)
 	for i in range(H):
 		lines += ["| {} | {} | {} | {} | {} |".format(frame.at[i, '题号'], frame.at[i, '标题'], frame.at[i, '题解'], frame.at[i, '标签'], frame.at[i, '难度'])]
@@ -39,114 +39,180 @@ def gen_markdown_table(frame):
 def create_categories_list(solotions_path, input_path, output_path):
 	f = open(input_path)
 	lines = f.readlines()
+	category_h2 = None
+	category_h3 = None
+	category_h4 = None
+	category_h6 = None
+	category_h3_file_path = None
+	category_h3_file_content = ""
+	
 	for i in range(len(lines)):
-		pattern = re.compile(r'\s*(#{2,6})\s*(.*?)\s*(?:\1)\s+')
+		pattern = re.compile(r'(#{2,6}) (.*)')
 		match = pattern.match(lines[i])
 		if match:
-			print(match.groups())
+			title_size, title_content =  match.group(1,2)
+			if title_size == "##":
+				category_h2 = title_content
+			elif title_size == "###":
+				if category_h3 and category_h3_file_path and category_h3_file_content:
+#					print(category_h3_file_content)
+					with open(category_h3_file_path, 'w') as fi:
+						fi.write(category_h3_file_content)
+					fi.close()
+					category_h3 = None
+					category_h3_file_path = None
+					category_h3_file_content = ""
+				pattern1 = re.compile(r'\[(.*)\]\((.*)\)')
+				match1 = pattern1.match(title_content)
+				if match1:
+					category_h3, category_h3_file_path = match1.group(1,2)
+					category_h3_file_content += "### " + category_h3 + "\n\n"
+			elif title_size == "####":
+				category_h4 = title_content
+				category_h3_file_content += "#### " + category_h4 + "\n\n"
+			elif title_size == "######":
+				category_h6 = title_content
+				problem_ids = title_content.split('、')
+				if not problem_ids:
+					continue
 				
-
-def create_list(solotions_path, output_path):
-	files =  os.listdir(solotions_path)
-	frame = pd.DataFrame(columns=['题号', '标题', '题解', '标签', '难度'])
-	frame_cout = 0
-	for file in files:
-		if not os.path.isdir(file) and ".md" in file: #判断是否是文件夹   
-			f = open(solotions_path + "/" + file)
-			
-			lines = f.readlines()
-			title_id = None
-			title_offer_id = None
-			title_offer_id1 = None
-			title_offer_id2 = None
-			title_name = None
-			title_solution_url = None
-			title_url = None
-			title_label = None
-			title_diff = None
-			
-			for i in range(len(lines)):
-				if i == 0:
-					pattern_1 = re.compile(r'\[剑指 Offer ([0-9]\d*|0)+( - [I]*)*\. (.*)\]\((.*)\)')
-					if re.search(pattern_1, lines[i]):
-						match_1 = pattern_1.finditer(lines[i])
-						for a in match_1:
-#							print(a)
-							title_offer_id1, title_offer_id2, title_name, title_url = a.group(1,2,3,4)
-							if title_offer_id2:
-								title_offer_id = "剑指 Offer " + title_offer_id1 + title_offer_id2
-							else:
-								title_offer_id = "剑指 Offer " + title_offer_id1
-						continue
+				frame = pd.DataFrame(columns=['题号', '标题', '题解', '标签', '难度'])
+				frame_cout = 0
+				for problem_id in problem_ids:
+					problem_id_path = solotions_path + "/" + problem_id + ".md"
+					res = get_problem_id_row(problem_id_path, problem_id)
+					if res:
+						frame.loc[frame_cout] = res
+						frame_cout += 1
+				table = gen_markdown_table(frame)
+				category_h3_file_content += table + "\n\n"
+	
+	if category_h3 and category_h3_file_path and category_h3_file_content:
+		with open(category_h3_file_path, 'w') as fi:
+			fi.write(category_h3_file_content)
+		fi.close()
+				
+				
 					
-					pattern_2 = re.compile(r'\[剑指 Offer II ([0-9]\d*|0)+\. (.*)\]\((.*)\)')
-					if re.search(pattern_2, lines[i]):
-						match_2 = pattern_2.finditer(lines[i])
-						for a in match_2:
-							print(a)
-							title_offer_id1, title_name, title_url = a.group(1,2,3)
-							title_offer_id = "剑指 Offer II " + title_offer_id1
-						continue
-					
-					pattern_3 = re.compile(r'\[面试题 ([0-9]\d*|0)+\.+([0-9]\d*|0)+\. (.*)\]\((.*)\)')
-					if re.search(pattern_3, lines[i]):
-						match_3 = pattern_3.finditer(lines[i])
-						for a in match_3:
-							print(a)
-							title_offer_id1, title_offer_id2, title_name, title_url = a.group(1,2,3,4)
-							title_offer_id = "面试题 " + title_offer_id1 + "." + title_offer_id2
-						continue
-						
-					
-					pattern = re.compile(r'\[([0-9]\d*|0)+\. (.*)\]\((.*)\)')
-					match = pattern.finditer(lines[i])
-					for a in match:
-						print(a)
-						title_id, title_name, title_url = a.group(1,2,3)
-				elif "标签" in lines[i]:
-					pattern = re.compile(r'- 标签：(.*)')
-					match = pattern.finditer(lines[i])
-					for a in match:
-						title_label = a.group(1)
-				elif "难度" in lines[i]:
-					pattern = re.compile(r'- 难度：(.*)')
-					match = pattern.finditer(lines[i])
-					for a in match:
-						title_diff = a.group(1)
-			if not title_diff:
-				title_diff = "简单"
-			if not title_label:
-				title_label = " "
-			
-			if title_offer_id and title_name and title_url and title_label and title_diff:					
-				title_chinese = quote(title_offer_id + ". " + title_name + ".md")
-				title_solution_url = "[Python](https://github.com/itcharge/LeetCode-Py/blob/main/Solutions/" + title_chinese + ")"
-				title_name_url = "[" + title_name + "](" + title_url + ")"
-				
-				frame.loc[frame_cout] = [title_offer_id, title_name_url, title_solution_url, title_label, title_diff]
-				frame_cout += 1
-				print(frame_cout, title_offer_id, title_name_url, title_url, title_label, title_diff, title_solution_url)
-			elif title_id and title_name and title_url and title_label and title_diff:
-				title_id = "{:0>4d}".format(int(title_id))
-				title_chinese = quote(title_id + ". " + title_name + ".md")
-				title_solution_url = "[Python](https://github.com/itcharge/LeetCode-Py/blob/main/Solutions/" + title_chinese + ")"
-				title_name_url = "[" + title_name + "](" + title_url + ")"
-				
-				frame.loc[frame_cout] = [title_id, title_name_url, title_solution_url, title_label, title_diff]
-				frame_cout += 1
-				print(frame_cout, title_id, title_name_url, title_url, title_label, title_diff, title_solution_url)	
-				
-				
-			f.close()
+def get_problem_id_row(problem_id_path, problem_id):
+	title_id = None
+	title_offer_id = None
+	title_offer_id1 = None
+	title_offer_id2 = None
+	title_name = None
+	title_solution_url = None
+	title_url = None
+	title_label = None
+	title_diff = None
+	res = None
+	
+	if not os.path.exists(problem_id_path):
+		title_offer_id = ""
+		pattern_1 = re.compile(r'剑指 Offer ([0-9]\d*|0)+( - [I]*)*\. (.*)')
+		pattern_2 = re.compile(r'剑指 Offer II ([0-9]\d*|0)+\. (.*)')
+		pattern_3 = re.compile(r'面试题 ([0-9]\d*|0)+\.+([0-9]\d*|0)+\. (.*)')
+		pattern_4 = re.compile(r'([0-9]\d*|0)+\. (.*)')
 		
-	table = gen_markdown_table(frame)
-	with open(output_path, 'w') as f:
-		f.writelines("# LeetCode 题解（已完成 {} 道）\n\n".format(frame_cout))
-		f.write(table)
-	f.close()
-	print("Create success")
-	return frame_cout
+		if re.search(pattern_1, problem_id):
+			match_1 = pattern_1.finditer(problem_id)
+			for a in match_1:
+				title_offer_id1, title_offer_id2, title_name = a.group(1,2,3)
+				if title_offer_id2:
+					title_offer_id = "剑指 Offer " + title_offer_id1 + title_offer_id2
+				else:
+					title_offer_id = "剑指 Offer " + title_offer_id1
+		elif re.search(pattern_2, problem_id):
+			match_2 = pattern_2.finditer(problem_id)
+			for a in match_2: 
+				title_offer_id1, title_name = a.group(1,2)
+				title_offer_id = "剑指 Offer II " + title_offer_id1
+		elif re.search(pattern_3, problem_id):
+			match_3 = pattern_3.finditer(problem_id)
+			for a in match_3:
+				title_offer_id1, title_offer_id2, title_name = a.group(1,2,3)
+				title_offer_id = "面试题 " + title_offer_id1 + "." + title_offer_id2
+		elif re.search(pattern_4, problem_id):
+			match_4 = pattern_4.finditer(problem_id)
+			for a in match_4:
+				title_offer_id, title_name = a.group(1,2)
+		
+		title_chinese = " "
+		title_solution_url = " "
+		title_name_url = title_name
+		title_label = " "
+		title_diff = " "
+		res = [title_offer_id, title_name_url, title_solution_url, title_label, title_diff]
+		return res
+	
+	f = open(problem_id_path)
+	lines = f.readlines()
+	for i in range(len(lines)):
+		if i == 0:
+			pattern_1 = re.compile(r'\[剑指 Offer ([0-9]\d*|0)+( - [I]*)*\. (.*)\]\((.*)\)')
+			pattern_2 = re.compile(r'\[剑指 Offer II ([0-9]\d*|0)+\. (.*)\]\((.*)\)')
+			pattern_3 = re.compile(r'\[面试题 ([0-9]\d*|0)+\.+([0-9]\d*|0)+\. (.*)\]\((.*)\)')
+			pattern_4 = re.compile(r'\[([0-9]\d*|0)+\. (.*)\]\((.*)\)')
+			if re.search(pattern_1, lines[i]):
+				match_1 = pattern_1.finditer(lines[i])
+				for a in match_1:
+					title_offer_id1, title_offer_id2, title_name, title_url = a.group(1,2,3,4)
+					if title_offer_id2:
+						title_offer_id = "剑指 Offer " + title_offer_id1 + title_offer_id2
+					else:
+						title_offer_id = "剑指 Offer " + title_offer_id1
+			elif re.search(pattern_2, lines[i]):
+				match_2 = pattern_2.finditer(lines[i])
+				for a in match_2: 
+					title_offer_id1, title_name, title_url = a.group(1,2,3)
+					title_offer_id = "剑指 Offer II " + title_offer_id1
+			elif re.search(pattern_3, lines[i]):
+				match_3 = pattern_3.finditer(lines[i])
+				for a in match_3:
+					title_offer_id1, title_offer_id2, title_name, title_url = a.group(1,2,3,4)
+					title_offer_id = "面试题 " + title_offer_id1 + "." + title_offer_id2
+			else:
+				match_4 = pattern_4.finditer(lines[i])
+				for a in match_4:
+					title_id, title_name, title_url = a.group(1,2,3)
+		elif "标签" in lines[i]:
+			pattern = re.compile(r'- 标签：(.*)')
+			match = pattern.finditer(lines[i])
+			for a in match:
+				title_label = a.group(1)
+		elif "难度" in lines[i]:
+			pattern = re.compile(r'- 难度：(.*)')
+			match = pattern.finditer(lines[i])
+			for a in match:
+				title_diff = a.group(1)
+	if not title_diff:
+		title_diff = "简单"
+	if not title_label:
+		title_label = " "
+		
+	if title_offer_id and title_name and title_url and title_label and title_diff:					
+		title_chinese = quote(title_offer_id + ". " + title_name + ".md")
+		title_solution_url = "[Python](https://github.com/itcharge/LeetCode-Py/blob/main/Solutions/" + title_chinese + ")"
+		title_name_url = "[" + title_name + "](" + title_url + ")"
+		
+		res = [title_offer_id, title_name_url, title_solution_url, title_label, title_diff]
+	elif title_id and title_name and title_url and title_label and title_diff:
+		title_id = "{:0>4d}".format(int(title_id))
+		title_chinese = quote(title_id + ". " + title_name + ".md")
+		title_solution_url = "[Python](https://github.com/itcharge/LeetCode-Py/blob/main/Solutions/" + title_chinese + ")"
+		title_name_url = "[" + title_name + "](" + title_url + ")"
 
+		res = [title_id, title_name_url, title_solution_url, title_label, title_diff]
+	else:
+		title_id = problem_id
+		title_chinese = " "
+		title_solution_url = " "
+		title_name_url = " "
+		title_label = " "
+		title_diff = " "
+		res = [title_id, title_name_url, title_solution_url, title_label, title_diff]
+	f.close()
+	return res
+	
 def merge_file(list_path, readme_head_path, readme_path, solutions_count):
 	readme_head_file = open(readme_head_path)
 	list_file = open(list_path)
