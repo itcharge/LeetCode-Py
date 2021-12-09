f5f2a2056c95324e35d9b3714e6694bfa70d8f89
ITCharge <i@itcharge.cn>
2021-12-09 22:35:56 +0800

    Create LinkedList-InsertionSort.py

---
+++ Templates/LinkedList-InsertionSort.py
@@ -0,0 +1,31 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def insertionSort(self, head: ListNode):
+        if not head and not head.next:
+            return head
+        
+        dummy_head = ListNode(-1)
+        dummy_head.next = head
+        sorted_list = head
+        cur = head.next 
+        
+        while cur:
+            if sorted_list.val <= cur.val:
+                sorted_list = sorted_list.next 
+            else:
+                prev = dummy_head
+                while prev.next.val <= cur.val:
+                    prev = prev.next
+                sorted_list.next = cur.next
+                cur.next = prev.next
+                prev.next = curr
+            cur = sorted_list.next 
+        
+        return dummy_head.next
+    
+    def sortLinkedList(self, head: ListNode):
+        return self.insertionSort(head)
