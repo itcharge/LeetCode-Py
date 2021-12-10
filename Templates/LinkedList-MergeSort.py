2c2345e31d0e7b9457dc0d95e97afe554f784f4f
ITCharge <i@itcharge.cn>
2021-12-10 17:56:59 +0800

    Update LinkedList-MergeSort.py

--- Templates/LinkedList-MergeSort.py
+++ Templates/LinkedList-MergeSort.py
@@ -5,6 +5,7 @@ class ListNode:
 
 class Solution:
     def merge(self, left, right):
+        # 归并环节
         dummy_head = ListNode(-1)
         cur = dummy_head
         while left and right:
@@ -24,17 +25,21 @@ class Solution:
         return dummy_head.next
         
     def mergeSort(self, head: ListNode):
+        # 分割环节
         if not head or not head.next:
             return head
         
+        # 快慢指针找到中心链节点
         slow, fast = head, head.next
         while fast and fast.next:
             slow = slow.next 
             fast = fast.next.next 
         
+        # 断开左右链节点
         left_head, right_head = head, slow.next 
         slow.next = None
         
+        # 归并操作
         return self.merge(self.mergeSort(left_head), self.mergeSort(right_head))
     
     
