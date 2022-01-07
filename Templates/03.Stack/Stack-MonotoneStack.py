8e81f5d83a6492528faae16b3a537e455b5d63bf
ITCharge <i@itcharge.cn>
2022-01-07 15:38:54 +0800

    Update Stack-MonotoneStack.py

--- Templates/03.Stack/Stack-MonotoneStack.py
+++ Templates/03.Stack/Stack-MonotoneStack.py
@@ -11,9 +11,30 @@ def monotoneStack(nums):
 		stack.append(num)
 		print(str(num) + " 入栈 " + str(stack))
 		
+def monotoneIncreasingStack(nums):
+	stack = []
+	for num in nums:
+		while stack and num >= stack[-1]:
+			top = stack[-1]
+			stack.pop()
+			print(str(top) + " 出栈 " + str(stack))
+		stack.append(num)
+		print(str(num) + " 入栈 " + str(stack))
+		
+def monotoneDecreasingStack(nums):
+	stack = []
+	for num in nums:
+		while stack and num <= stack[-1]:
+			top = stack[-1]
+			stack.pop()
+			print(str(top) + " 出栈 " + str(stack))
+		stack.append(num)
+		print(str(num) + " 入栈 " + str(stack))
+		
+
 nums = []
 for i in range(8):
 	nums.append(random.randint(1, 9))
-
-nums = [4, 3, 2, 5, 7, 4, 6, 8]
-monotoneStack(nums)
+print(nums)
+#nums = [4, 3, 2, 5, 7, 4, 6, 8]
+monotoneIncreasingStack(nums)
