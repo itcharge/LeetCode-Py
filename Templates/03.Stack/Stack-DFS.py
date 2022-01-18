c3e9c17dcb5e06d2396d168b9a66093c997231cc
ITCharge <i@itcharge.cn>
2022-01-18 09:24:17 +0800

    Update Stack-DFS.py

--- Templates/03.Stack/Stack-DFS.py
+++ Templates/03.Stack/Stack-DFS.py
@@ -8,18 +8,26 @@ def dfs_recursive(graph, start, visited):
             dfs_recursive(graph, end, visited)
 
 def dfs_stack(graph, start):
-    visited = set(start)
-    stack = [start]
-
+    print(start)                        # 访问节点 start
+    visited = set(start)                # 使用 visited 标记访问过的节点，先标记 start
+    stack = [start]                     # 创建一个栈，并将 start 加入栈中
+    
     while stack:
-        node_u = stack.pop()
-        # 访问节点
-        print(node_u)
-        for node_v in graph[node_u]:
-            if node_v not in visited:
-                stack.append(node_v)
-                visited.add(node_v)
+        node_u = stack[-1]              # 取栈顶元素
         
+        i = 0
+        while i < len(graph[node_u]):   # 遍历栈顶元素，遇到未访问节点，访问节点并跳出。
+            node_v = graph[node_u][i]
+            
+            if node_v not in visited:   # node_v 未访问过
+                print(node_v)           # 访问节点 node_v
+                stack.append(node_v)    # 将 node_v 加入栈中
+                visited.add(node_v)     # 标记为访问过 node_v
+                break
+            i += 1
+        
+        if i == len(graph[node_u ]):    # node_u 相邻的节点都访问结束了，弹出 node_u
+            stack.pop()
 
 graph = {
     "A": ["B", "C"],
@@ -31,8 +39,8 @@ graph = {
 }
 
 # 基于递归实现的深度优先搜索
-visited = set()
-dfs_recursive(graph, "A", visited)
+#visited = set()
+#dfs_recursive(graph, "A", visited)
 
 
 # 基于堆栈实现的深度优先搜索
