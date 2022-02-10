ef74d8039bf93f7c53ad2b1c89d846328156644f
ITCharge <i@itcharge.cn>
2022-02-10 14:25:14 +0800

    Update String-TrieTree.py

--- Templates/06.String/String-TrieTree.py
+++ Templates/06.String/String-TrieTree.py
@@ -8,27 +8,27 @@ class Trie:
     # 向字典树中插入一个单词
     def insert(self, word: str) -> None:
         cur = self
-        for ch in word:
-            if ch not in cur.children:
-                cur.children[ch] = Trie()
-            cur = cur.children[ch]
-        cur.isEnd = True
+        for ch in word:                         # 遍历单词中的字符
+            if ch not in cur.children:          # 如果当前节点的子节点中，不存在键为 ch 的节点
+                cur.children[ch] = Trie()       # 建立一个节点，并将其保存到当前节点的子节点
+            cur = cur.children[ch]              # 令当前节点指向新建立的节点，继续处理下一个字符
+        cur.isEnd = True                        # 单词处理完成时，将当前节点标记为单词结束
 
     # 查找字典树中是否存在一个单词
     def search(self, word: str) -> bool:
         cur = self
-        for ch in word:
-            if ch not in cur.children:
-                return False
-            cur = cur.children[ch]
+        for ch in word:                         # 遍历单词中的字符
+            if ch not in cur.children:          # 如果当前节点的子节点中，不存在键为 ch 的节点
+                return False                    # 直接返回 False
+            cur = cur.children[ch]              # 令当前节点指向新建立的节点，然后继续查找下一个字符
 
-        return cur is not None and cur.isEnd
+        return cur is not None and cur.isEnd    # 判断当前节点是否为空，并且是否有单词结束标记
 
     # 查找字典树中是否存在一个前缀
     def startsWith(self, prefix: str) -> bool:
         cur = self
-        for ch in prefix:
-            if ch not in cur.children:
-                return False
-            cur = cur.children[ch]
-        return cur is not None
+        for ch in prefix:                       # 遍历前缀中的字符
+            if ch not in cur.children:          # 如果当前节点的子节点中，不存在键为 ch 的节点
+                return False                    # 直接返回 False
+            cur = cur.children[ch]              # 令当前节点指向新建立的节点，然后继续查找下一个字符
+        return cur is not None                  # 判断当前节点是否为空，不为空则查找成功
