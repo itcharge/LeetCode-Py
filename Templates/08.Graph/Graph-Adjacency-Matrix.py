5d62e4551c9b7e2bea7610afd450088eddbabcd6
ITCharge <i@itcharge.cn>
2022-03-15 20:57:15 +0800

    Update Graph-Adjacency-Matrix.py

--- Templates/08.Graph/Graph-Adjacency-Matrix.py
+++ Templates/08.Graph/Graph-Adjacency-Matrix.py
@@ -1,15 +1,12 @@
 class Graph:                                    # 基本图类，采用邻接矩阵表示
     # 图的初始化操作，vertices 为顶点
-    def __init__(self, vertices=[]):
-        self.ver_count = len(vertices)          # 顶点个数
-        self.vertices = vertices                # 顶点数组
-        self.adj_matrix = [[None for _ in range(self.ver_count)] for _ in range(self.ver_count)]  # 邻接矩阵
+    def __init__(self, ver_count):
+        self.ver_count = ver_count              # 顶点个数
+        self.adj_matrix = [[None for _ in range(ver_count)] for _ in range(ver_count)]  # 邻接矩阵
     
     # 判断顶点 v 是否有效
     def __valid(self, v):
-        if v in self.vertices:
-            return True
-        return False
+        return 0 <= v <= self.ver_count
     
     # 图的创建操作，edges 为边信息
     def creatGraph(self, edges=[]):
@@ -21,29 +18,26 @@ class Graph:                                    # 基本图类，采用邻接矩
         if not self.__valid(vi) or not self.__valid(vj):
             raise ValueError(str(vi) + ' or ' + str(vj) + " is not a valid vertex.")
         
-        vi_index = self.vertices.index(vi)
-        vj_index = self.vertices.index(vj)
-        self.adj_matrix[vi_index][vj_index] = val
+        self.adj_matrix[vi][vj] = val
     
     # 获取 vi - vj 边的权值
     def get_edge(self, vi, vj):
         if not self.__valid(vi) or not self.__valid(vj):
             raise ValueError(str(vi) + ' or ' + str(vj) + " is not a valid vertex.")
-        
-        vi_index = self.vertices.index(vi)
-        vj_index = self.vertices.index(vj)
-        return self.adj_matrix[vi_index][vj_index]
+
+        return self.adj_matrix[vi][vj]
     
     # 根据邻接矩阵打印图的边
     def printGraph(self):
-        for vi in self.vertices:
-            for vj in self.vertices:
+        for vi in range(self.ver_count):
+            for vj in range(self.ver_count):
                 val = self.get_edge(vi, vj)
                 if val:
-                    print(vi + ' - ' + vj + ' : ' + str(val))
+                    print(str(vi) + ' - ' + str(vj) + ' : ' + str(val))
     
-vertices = ['v1', 'v2', 'v3', 'v4', 'v5']
-graph = Graph(vertices)
-edges = [['v1', 'v4', 3],['v1', 'v3', 9],['v3', 'v4', 6],['v2', 'v5', 4],['v4', 'v5', 2]]
+
+graph = Graph(6)
+edges = [[1, 4, 3],[1, 3, 9],[3, 4, 6],[2, 5, 4],[4, 5, 2]]
 graph.creatGraph(edges)
+print(graph.get_edge(3, 4))
 graph.printGraph()
