edf6b32a67198af0fcd7e2d165c8ea7165171f13
ITCharge <i@itcharge.cn>
2022-03-20 22:21:13 +0800

    Create Graph-Bellman-Ford.py

---
+++ Templates/08.Graph/Graph-Bellman-Ford.py
@@ -0,0 +1,33 @@
+class Solution:
+    def bellmanFord(self, graph, source):
+        size = len(graph)
+        dist = dict()
+        
+        for vi in graph:
+            dist[vi] = float('inf')
+        
+        dist[source] = 0
+        
+        for i in range(size - 1):
+            for vi in graph:
+                for vj in graph[vi]:
+                    if dist[vj] > graph[vi][vj] + dist[vi]:
+                        dist[vj] = graph[vi][vj] + dist[vi]
+        
+        for vi in graph:
+            for vj in graph[vi]:
+                if dist[vj] > dist[vi] + graph[vi][vj]:
+                    return None
+        
+        return dist
+                    
+                    
+graph = {
+    'a': {'b': -1, 'c':  4},
+    'b': {'c':  2, 'd':  3, 'e':  2},
+    'c': {},
+    'd': {'b':  3, 'c':  5},
+    'e': {'d': -3}
+}
+dist = Solution().bellmanFord(graph, 'a')
+print(dist)				
