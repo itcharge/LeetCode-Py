5c2dbe615c04c29f13d1b89471f333341ec7d98a
杨世超 <ysc.coder@gmail.com>
2022-04-14 21:44:29 +0800

    Update Array-MergeSort.py

--- Templates/01.Array/Array-MergeSort.py
+++ Templates/01.Array/Array-MergeSort.py
@@ -1,24 +1,26 @@
 class Solution:
-    def merge(self, left_arr, right_arr):
+    def merge(self, left_arr, right_arr):           # 合并
         arr = []
-        while left_arr and right_arr:
+        while left_arr and right_arr:               # 将两个排序数组中较小元素依次插入到结果数组中
             if left_arr[0] <= right_arr[0]:
                 arr.append(left_arr.pop(0))
             else:
                 arr.append(right_arr.pop(0))
-        while left_arr:
+                
+        while left_arr:                             # 如果左子序列有剩余元素，则将其插入到结果数组中
             arr.append(left_arr.pop(0))
-        while right_arr:
+        while right_arr:                            # 如果右子序列有剩余元素，则将其插入到结果数组中
             arr.append(right_arr.pop(0))
-        return arr
+        return arr                                  # 返回排好序的结果数组
 
-    def mergeSort(self, arr):
-        size = len(arr)
-        if size < 2:
+    def mergeSort(self, arr):                       # 分解
+        if len(arr) <= 1:                           # 数组元素个数小于等于 1 时，直接返回原数组
             return arr
-        mid = len(arr) // 2
-        left_arr, right_arr = arr[0: mid], arr[mid:]
-        return self.merge(self.mergeSort(left_arr), self.mergeSort(right_arr))
+        
+        mid = len(arr) // 2                         # 将数组从中间位置分为左右两个数组。
+        left_arr = self.mergeSort(arr[0: mid])      # 递归将左子序列进行分解和排序
+        right_arr =  self.mergeSort(arr[mid:])      # 递归将右子序列进行分解和排序
+        return self.merge(left_arr, right_arr)      # 把当前序列组中有序子序列逐层向上，进行两两合并。
 
     def sortArray(self, nums: List[int]) -> List[int]:
         return self.mergeSort(nums)
