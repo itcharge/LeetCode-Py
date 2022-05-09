ffab9eb1c60e086380616e75017c06cfc2ea9a7b
杨世超 <ysc.coder@gmail.com>
2022-05-09 11:37:01 +0800

    更新 并查集文章

--- Templates/07.Tree/Tree-UnionFind.py
+++ Templates/07.Tree/Tree-UnionFind.py
@@ -1,21 +1,20 @@
 class UnionFind:
+    def __init__(self, n):                          # 初始化
+        self.fa = [i for i in range(n)]             # 每个元素的集合编号初始化为数组 fa 的下标索引
+    
+    def find(self, x):                              # 查找元素根节点的集合编号内部实现方法
+        while self.fa[x] != x:                      # 递归查找元素的父节点，直到根节点
+            self.fa[x] = self.fa[self.fa[x]]        # 隔代压缩优化
+            x = self.fa[x]
+        return x                                    # 返回元素根节点的集合编号
 
-    def __init__(self, n):
-        self.parent = [i for i in range(n)]
-
-    def find(self, x):
-        while x != self.parent[x]:
-            self.parent[x] = self.parent[self.parent[x]]
-            x = self.parent[x]
-        return x
-
-    def union(self, x, y):
+    def union(self, x, y):                          # 合并操作：令其中一个集合的树根节点指向另一个集合的树根节点
         root_x = self.find(x)
         root_y = self.find(y)
-        if root_x == root_y:
+        if root_x == root_y:                        # x 和 y 的根节点集合编号相同，说明 x 和 y 已经同属于一个集合
             return False
-        self.parent[root_x] = root_y
+        self.fa[root_x] = root_y                    # x 的根节点连接到 y 的根节点上，成为 y 的根节点的子节点
         return True
 
-    def is_connected(self, x, y):
+    def is_connected(self, x, y):                   # 查询操作：判断 x 和 y 是否同属于一个集合
         return self.find(x) == self.find(y)
