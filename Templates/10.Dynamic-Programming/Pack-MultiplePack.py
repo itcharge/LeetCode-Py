34ab7ce307302b0e254da07c545dfaf06ac8dbe9
ITCharge <i@itcharge.cn>
2023-03-21 23:03:12 +0800

    Update Pack-MultiplePack.py

--- Templates/10.Dynamic-Programming/Pack-MultiplePack.py
+++ Templates/10.Dynamic-Programming/Pack-MultiplePack.py
@@ -1,5 +1,5 @@
 class Solution:
-    def multiplePack(self, weight: [int], value: [int], count: [int], W: int):
+    def multiplePackMethod1(self, weight: [int], value: [int], count: [int], W: int):
         size = len(weight)
         dp = [[0 for _ in range(W + 1)] for _ in range(size + 1)]
         
@@ -13,3 +13,18 @@ class Solution:
                     dp[i][w] = max(dp[i][w], dp[i - 1][w - k * weight[i - 1]] + k * value[i - 1])
                     
         return dp[size][W]
+    
+    def multiplePackMethod2(self, weight: [int], value: [int], count: [int], W: int):
+        size = len(weight)
+        dp = [0 for _ in range(W + 1)]
+        
+        # 枚举前 i 种物品
+        for i in range(1, size + 1):
+            # 逆序枚举背包装载重量（避免状态值错误）
+            for w in range(W, weight[i - 1] - 1, -1):
+                # 枚举第 i 种物品能取个数
+                for k in range(min(count[i - 1], w // weight[i - 1]) + 1):
+                    # dp[w] 取所有 dp[w - k * weight[i - 1]] + k * value[i - 1] 中最大值
+                    dp[w] = max(dp[w], dp[w - k * weight[i - 1]] + k * value[i - 1])
+                
+        return dp[W]
