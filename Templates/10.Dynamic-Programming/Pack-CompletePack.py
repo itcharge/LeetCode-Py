68394cbe9f0c424e0b74eac947743490cad9611c
ITCharge <i@itcharge.cn>
2023-03-21 16:36:29 +0800

    Update Pack-CompletePack.py

--- Templates/10.Dynamic-Programming/Pack-CompletePack.py
+++ Templates/10.Dynamic-Programming/Pack-CompletePack.py
@@ -24,10 +24,10 @@ class Solution:
             for w in range(1, W + 1):
                 # 第 i - 1 件物品装不下
                 if w < weight[i - 1]:
-                    # dp[i][w] 取「前 i - 1 件物品装入载重为 w 的背包中的最大价值」
+                    # dp[i][w] 取「前 i - 1 种物品装入载重为 w 的背包中的最大价值」
                     dp[i][w] = dp[i - 1][w]
                 else:
-                    # dp[i][w] 取「前 i - 1 件物品装入载重为 w 的背包中的最大价值」与「前 i 件物品装入载重为 w - weight[i - 1] 的背包中，再装入 1 件第 i - 1 物品所得的最大价值」两者中的最大值
+                    # dp[i][w] 取「前 i - 1 种物品装入载重为 w 的背包中的最大价值」与「前 i 种物品装入载重为 w - weight[i - 1] 的背包中，再装入 1 件第 i - 1 种物品所得的最大价值」两者中的最大值
                     dp[i][w] = max(dp[i - 1][w], dp[i][w - weight[i - 1]] + value[i - 1])
                     
         return dp[size][W]
@@ -41,6 +41,7 @@ class Solution:
         for i in range(1, size + 1):
             # 正序枚举背包装载重量
             for w in range(weight[i - 1], W + 1):
+                # dp[w] 取「前 i - 1 种物品装入载重为 w 的背包中的最大价值」与「前 i 种物品装入载重为 w - weight[i - 1] 的背包中，再装入 1 件第 i - 1 种物品所得的最大价值」两者中的最大值
                 dp[w] = max(dp[w], dp[w - weight[i - 1]] + value[i - 1])
                 
         return dp[W]
