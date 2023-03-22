26a49610d8ac3a8446185522428c3c38da9fa981
ITCharge <i@itcharge.cn>
2023-03-22 17:40:53 +0800

    Update Pack-MultiplePack.py

--- Templates/10.Dynamic-Programming/Pack-MultiplePack.py
+++ Templates/10.Dynamic-Programming/Pack-MultiplePack.py
@@ -1,4 +1,5 @@
 class Solution:
+    # 思路 1：动态规划 + 二维基本思路
     def multiplePackMethod1(self, weight: [int], value: [int], count: [int], W: int):
         size = len(weight)
         dp = [[0 for _ in range(W + 1)] for _ in range(size + 1)]
@@ -14,6 +15,7 @@ class Solution:
                     
         return dp[size][W]
     
+    # 思路 2：动态规划 + 滚动数组优化
     def multiplePackMethod2(self, weight: [int], value: [int], count: [int], W: int):
         size = len(weight)
         dp = [0 for _ in range(W + 1)]
@@ -27,4 +29,32 @@ class Solution:
                     # dp[w] 取所有 dp[w - k * weight[i - 1]] + k * value[i - 1] 中最大值
                     dp[w] = max(dp[w], dp[w - k * weight[i - 1]] + k * value[i - 1])
                 
+        return dp[W]
+    
+    # 思路 3：动态规划 + 二进制优化
+    def multiplePackMethod3(self, weight: [int], value: [int], count: [int], W: int):
+        weight_new, value_new = [], []
+        
+        for i in range(len(weight)):
+            cnt = count[i]
+            k = 1
+            while k <= cnt:
+                cnt -= k
+                weight_new.append(weight[i] * k)
+                value_new.append(value[i] * k)
+                k *= 2
+            if cnt > 0:
+                weight_new.append(weight[i] * cnt)
+                value_new.append(value[i] * cnt)
+        
+        dp = [0 for _ in range(W + 1)]
+        size = len(weight_new)
+        
+        # 枚举前 i 种物品
+        for i in range(1, size + 1):
+            # 逆序枚举背包装载重量（避免状态值错误）
+            for w in range(W, weight_new[i - 1] - 1, -1):
+                # dp[w] 取「前 i - 1 件物品装入载重为 w 的背包中的最大价值」与「前 i - 1 件物品装入载重为 w - weight_new[i - 1] 的背包中，再装入第 i - 1 物品所得的最大价值」两者中的最大值
+                dp[w] = max(dp[w], dp[w - weight_new[i - 1]] + value_new[i - 1])
+                    
         return dp[W]
