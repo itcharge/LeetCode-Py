3b50afc603f77a825dbc3de0f3967d82b7a6e39b
ITCharge <i@itcharge.cn>
2023-05-05 09:10:29 +0800

    Update Graph-Topological-Sorting-Kahn.py

--- Templates/08.Graph/Graph-Topological-Sorting-Kahn.py
+++ Templates/08.Graph/Graph-Topological-Sorting-Kahn.py
@@ -1,4 +1,25 @@
+import collections
+
 class solution:
     def topologicalSorting(graph):
-        indegrees = []
-        for 
+        indegrees = {u: 0 for u in graph}
+        for u in graph:
+            for v in graph[u]:
+                indegrees[v] += 1
+        
+        
+        S = collections.deque([u for u in indegrees if indegrees[u] == 0])
+        order = []
+        
+        while S:
+            u = S.pop()
+            order.append(u)
+            for v in graph[u]:
+                indegrees[v] -= 1
+                if indegrees[v] == 0:
+                    S.append(v)
+        
+        size = len(indegrees)
+        if size == len(S):
+            return order
+        return None
