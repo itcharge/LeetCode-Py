e2924cbf0f64b5eaf0be64449ed208e59bdd1eb8
ITCharge <i@itcharge.cn>
2023-08-09 18:07:24 +0800

    Update Array-BubbleSort.py

--- Templates/01.Array/Array-BubbleSort.py
+++ Templates/01.Array/Array-BubbleSort.py
@@ -1,7 +1,7 @@
 class Solution:
     def bubbleSort(self, arr):
         # 第 i 趟排序
-        for i in range(len(arr)):
+        for i in range(len(arr) - 1):
             # 从序列中前 n - i + 1 个元素的第 1 个元素开始，相邻两个元素进行比较
             for j in range(len(arr) - i - 1):
                 # 相邻两个元素进行比较，如果前者大于后者，则交换位置
