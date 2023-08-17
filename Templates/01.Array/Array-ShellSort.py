d2e3e4369a9f54b6e6bf1f525fa68cdfa707b3ee
ITCharge <i@itcharge.cn>
2023-08-17 18:05:18 +0800

    Update Array-ShellSort.py

--- Templates/01.Array/Array-ShellSort.py
+++ Templates/01.Array/Array-ShellSort.py
@@ -1,24 +1,26 @@
 class Solution:
-    def shellSort(self, arr):
-        size = len(arr)
+    def shellSort(self, nums: [int]) -> [int]:
+        size = len(nums)
         gap = size // 2
         # 按照 gap 分组
         while gap > 0:
             # 对每组元素进行插入排序
             for i in range(gap, size):
-                # temp 为每组中无序序列第 1 个元素
-                temp = arr[i]
+                # temp 为每组中无序数组第 1 个元素
+                temp = nums[i]
                 j = i
-                # 从右至左遍历每组中的有序序列元素
-                while j >= gap and arr[j - gap] > temp:
-                    # 将每组有序序列中插入位置右侧的元素依次在组中右移一位
-                    arr[j] = arr[j - gap]
+                # 从右至左遍历每组中的有序数组元素
+                while j >= gap and nums[j - gap] > temp:
+                    # 将每组有序数组中插入位置右侧的元素依次在组中右移一位
+                    nums[j] = nums[j - gap]
                     j -= gap
                 # 将该元素插入到适当位置
-                arr[j] = temp
+                nums[j] = temp
             # 缩小 gap 间隔
             gap = gap // 2
-        return arr
+        return nums
 
-    def sortArray(self, nums: List[int]) -> List[int]:
+    def sortArray(self, nums: [int]) -> [int]:
         return self.shellSort(nums)
+    
+print(Solution().sortArray([7, 2, 6, 8, 0, 4, 1, 5, 9, 3]))
