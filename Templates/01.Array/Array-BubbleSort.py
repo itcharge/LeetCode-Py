2cf0484cb239dfce9f767cf51195a55ae4db8377
ITCharge <i@itcharge.cn>
2023-08-17 18:05:02 +0800

    Update Array-BubbleSort.py

--- Templates/01.Array/Array-BubbleSort.py
+++ Templates/01.Array/Array-BubbleSort.py
@@ -1,20 +1,20 @@
 class Solution:
-    def bubbleSort(self, arr):
-        # 第 i 趟遍历
-        for i in range(len(arr) - 1):
+    def bubbleSort(self, nums: [int]) -> [int]:
+        # 第 i 趟「冒泡」
+        for i in range(len(nums) - 1):
             flag = False    # 是否发生交换的标志位
-            # 从序列中前 n - i + 1 个元素的第 1 个元素开始，相邻两个元素进行比较
-            for j in range(len(arr) - i - 1):
+            # 从数组中前 n - i + 1 个元素的第 1 个元素开始，相邻两个元素进行比较
+            for j in range(len(nums) - i - 1):
                 # 相邻两个元素进行比较，如果前者大于后者，则交换位置
-                if arr[j] > arr[j + 1]:
-                    arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                if nums[j] > nums[j + 1]:
+                    nums[j], nums[j + 1] = nums[j + 1], nums[j]
                     flag = True
             if not flag:    # 此趟遍历未交换任何元素，直接跳出
                 break
         
-        return arr
+        return nums
     
-    def sortArray(self, nums):
+    def sortArray(self, nums: [int]) -> [int]:
         return self.bubbleSort(nums)
     
     
