fafc64810c4b38d70a1d1d3828a71d070c95b9a3
ITCharge <i@itcharge.cn>
2023-08-17 18:05:12 +0800

    Update Array-QuickSort.py

--- Templates/01.Array/Array-QuickSort.py
+++ Templates/01.Array/Array-QuickSort.py
@@ -1,41 +1,51 @@
 import random
 
 class Solution:
-    # 从 arr[low: high + 1] 中随机挑选一个基准数，并进行移动排序
-    def randomPartition(self, arr: [int], low: int, high: int):
+    # 随机哨兵划分：从 nums[low: high + 1] 中随机挑选一个基准数，并进行移位排序
+    def randomPartition(self, nums: [int], low: int, high: int) -> int:
         # 随机挑选一个基准数
         i = random.randint(low, high)
         # 将基准数与最低位互换
-        arr[i], arr[low] = arr[low], arr[i]
-        # 以最低位为基准数，然后将序列中比基准数大的元素移动到基准数右侧，比他小的元素移动到基准数左侧。最后将基准数放到正确位置上
-        return self.partition(arr, low, high)
+        nums[i], nums[low] = nums[low], nums[i]
+        # 以最低位为基准数，然后将数组中比基准数大的元素移动到基准数右侧，比他小的元素移动到基准数左侧。最后将基准数放到正确位置上
+        return self.partition(nums, low, high)
     
-    # 以最低位为基准数，然后将序列中比基准数大的元素移动到基准数右侧，比他小的元素移动到基准数左侧。最后将基准数放到正确位置上
-    def partition(self, arr: [int], low: int, high: int):
-        pivot = arr[low]            # 以第 1 为为基准数
-        i = low + 1                 # 从基准数后 1 位开始遍历，保证位置 i 之前的元素都小于基准数
+    # 哨兵划分：以第 1 位元素 nums[low] 为基准数，然后将比基准数小的元素移动到基准数左侧，将比基准数大的元素移动到基准数右侧，最后将基准数放到正确位置上
+    def partition(self, nums: [int], low: int, high: int) -> int:
+        if low >= high:
+            return nums
         
-        for j in range(i, high + 1):
-            # 发现一个小于基准数的元素
-            if arr[j] < pivot:
-                # 将小于基准数的元素 arr[j] 与当前 arr[i] 进行换位，保证位置 i 之前的元素都小于基准数
-                arr[i], arr[j] = arr[j], arr[i]
-                # i 之前的元素都小于基准数，所以 i 向右移动一位
+        # 以第 1 位元素为基准数
+        pivot = nums[low]
+        
+        i, j = low, high
+        while i < j:
+            # 从右向左找到第 1 个小于基准数的元素
+            while i < j and nums[j] >= pivot:
+                j -= 1
+            # 从左向右找到第 1 个大于基准数的元素
+            while i < j and nums[i] <= pivot:
                 i += 1
-        # 将基准节点放到正确位置上
-        arr[i - 1], arr[low] = arr[low], arr[i - 1]
-        # 返回基准数位置
-        return i - 1
+            # 交换元素
+            nums[i], nums[j] = nums[j], nums[i]
+        
+        # 将基准数放到正确位置上
+        nums[j], nums[low] = nums[low], nums[j]
+        # 返回基准数的索引
+        print(nums)
+        return j
 
-    def quickSort(self, arr, low, high):
+    def quickSort(self, nums: [int], low: int, high: int) -> [int]:
         if low < high:
-            # 按照基准数的位置，将序列划分为左右两个子序列
-            pi = self.randomPartition(arr, low, high)
-            # 对左右两个子序列分别进行递归快速排序
-            self.quickSort(arr, low, pi - 1)
-            self.quickSort(arr, pi + 1, high)
+            # 按照基准数的位置，将数组划分为左右两个子数组
+            pivot_i = self.partition(nums, low, high)
+            # 对左右两个子数组分别进行递归快速排序
+            self.quickSort(nums, low, pivot_i - 1)
+            self.quickSort(nums, pivot_i + 1, high)
 
-        return arr
+        return nums
 
-    def sortArray(self, nums: List[int]) -> List[int]:
+    def sortArray(self, nums: [int]) -> [int]:
         return self.quickSort(nums, 0, len(nums) - 1)
+    
+print(Solution().sortArray([4, 7, 5, 2, 6, 1, 3]))
