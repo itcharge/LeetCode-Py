5471e10ab237912964bf8525c3f77c266797be24
ITCharge <i@itcharge.cn>
2023-08-17 18:05:05 +0800

    Update Array-InsertionSort.py

--- Templates/01.Array/Array-InsertionSort.py
+++ Templates/01.Array/Array-InsertionSort.py
@@ -1,18 +1,20 @@
 class Solution:
-    def insertionSort(self, arr):
-        # 遍历无序序列
-        for i in range(1, len(arr)):
-            temp = arr[i]
+    def insertionSort(self, nums: [int]) -> [int]:
+        # 遍历无序区间
+        for i in range(1, len(nums)):
+            temp = nums[i]
             j = i
-            # 从右至左遍历有序序列
-            while j > 0 and arr[j - 1] > temp:
-                # 将有序序列中插入位置右侧的元素依次右移一位
-                arr[j] = arr[j - 1]
+            # 从右至左遍历有序区间
+            while j > 0 and nums[j - 1] > temp:
+                # 将有序区间中插入位置右侧的元素依次右移一位
+                nums[j] = nums[j - 1]
                 j -= 1
             # 将该元素插入到适当位置
-            arr[j] = temp
+            nums[j] = temp
 
-        return arr
+        return nums
 
-    def sortArray(self, nums: List[int]) -> List[int]:
+    def sortArray(self, nums: [int]) -> [int]:
         return self.insertionSort(nums)
+    
+print(Solution().sortArray([5, 2, 3, 6, 1, 4]))
