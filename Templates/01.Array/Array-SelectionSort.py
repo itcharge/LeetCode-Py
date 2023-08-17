65fc6e2abc52942375a2c98894efd948bc50c062
ITCharge <i@itcharge.cn>
2023-08-17 18:05:15 +0800

    Update Array-SelectionSort.py

--- Templates/01.Array/Array-SelectionSort.py
+++ Templates/01.Array/Array-SelectionSort.py
@@ -1,15 +1,17 @@
 class Solution:
-    def selectionSort(self, arr):
-        for i in range(len(arr) - 1):
-            # 记录未排序部分中最小值的位置
+    def selectionSort(self, nums: [int]) -> [int]:
+        for i in range(len(nums) - 1):
+            # 记录未排序区间中最小值的位置
             min_i = i
-            for j in range(i + 1, len(arr)):
-                if arr[j] < arr[min_i]:
+            for j in range(i + 1, len(nums)):
+                if nums[j] < nums[min_i]:
                     min_i = j
             # 如果找到最小值的位置，将 i 位置上元素与最小值位置上的元素进行交换
             if i != min_i:
-                arr[i], arr[min_i] = arr[min_i], arr[i]
-        return arr
+                nums[i], nums[min_i] = nums[min_i], nums[i]
+        return nums
 
-    def sortArray(self, nums: List[int]) -> List[int]:
+    def sortArray(self, nums: [int]) -> [int]:
         return self.selectionSort(nums)
+    
+print(Solution().sortArray([5, 2, 3, 6, 1, 4]))
