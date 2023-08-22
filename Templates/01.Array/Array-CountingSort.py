b706bffc877c5c8aed4dca8e7011fc761976c007
ITCharge <i@itcharge.cn>
2023-08-22 18:03:58 +0800

    Update Array-CountingSort.py

--- Templates/01.Array/Array-CountingSort.py
+++ Templates/01.Array/Array-CountingSort.py
@@ -1,28 +1,31 @@
 class Solution:
-    def countingSort(self, arr):
-        # 计算待排序序列中最大值元素 arr_max 和最小值元素 arr_min
-        arr_min, arr_max = min(arr), max(arr)
+    def countingSort(self, nums: [int]) -> [int]:
+        # 计算待排序数组中最大值元素 nums_max 和最小值元素 nums_min
+        nums_min, nums_max = min(nums), max(nums)
         # 定义计数数组 counts，大小为 最大值元素 - 最小值元素 + 1
-        size = arr_max - arr_min + 1
+        size = nums_max - nums_min + 1
         counts = [0 for _ in range(size)]
         
         # 统计值为 num 的元素出现的次数
-        for num in arr:
-            counts[num - arr_min] += 1
+        for num in nums:
+            counts[num - nums_min] += 1
         
-        # 计算元素排名
-        for j in range(1, size):
-            counts[j] += counts[j - 1]
+        # 生成累积计数数组
+        for i in range(1, size):
+            counts[i] += counts[i - 1]
 
         # 反向填充目标数组
-        res = [0 for _ in range(len(arr))]
-        for i in range(len(arr) - 1, -1, -1):
-            # 根据排名，将 arr[i] 放在数组对应位置
-            res[counts[arr[i] - arr_min] - 1] = arr[i]
-            # 将 arr[i] 的对应排名减 1
-            counts[arr[i] - arr_min] -= 1
+        res = [0 for _ in range(len(nums))]
+        for i in range(len(nums) - 1, -1, -1):
+            num = nums[i]
+            # 根据累积计数数组，将 num 放在数组对应位置
+            res[counts[num - nums_min] - 1] = num
+            # 将 num 的对应放置位置减 1，从而得到下个元素 num 的放置位置
+            counts[nums[i] - nums_min] -= 1
 
         return res
 
-    def sortArray(self, nums: List[int]) -> List[int]:
+    def sortArray(self, nums: [int]) -> [int]:
         return self.countingSort(nums)
+    
+print(Solution().sortArray([3, 0, 4, 2, 5, 1, 3, 1, 4, 5]))
