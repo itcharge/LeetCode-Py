942a69f498a0c905326d6c95554b915df6043880
ITCharge <i@itcharge.cn>
2023-08-22 18:13:34 +0800

    Update Array-RadixSort.py

--- Templates/01.Array/Array-RadixSort.py
+++ Templates/01.Array/Array-RadixSort.py
@@ -5,7 +5,7 @@ class Solution:
         
         # 从最低位（个位）开始，逐位遍历每一位
         for i in range(size):
-            # 定义个数为 10 的桶数组 buckets，每个桶分别代表 0 ~ 9 中的 1 个数字。
+            # 定义长度为 10 的桶数组 buckets，每个桶分别代表 0 ~ 9 中的 1 个数字。
             buckets = [[] for _ in range(10)]
             # 遍历数组元素，按照每个元素当前位上的数字，将元素放入对应数字的桶中。
             for num in nums:
