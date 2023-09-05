db4f1c3daeee3e7f7dfbf23f348a1b1f16423702
ITCharge <i@itcharge.cn>
2023-09-05 16:10:15 +0800

    Update Graph-BFS.py

--- Templates/08.Graph/Graph-BFS.py
+++ Templates/08.Graph/Graph-BFS.py
@@ -1,29 +1,31 @@
 import collections
 
-def bfs(graph, start):
-    visited = set()
-    queue = collections.deque([])
-    
-    visited.add(start)
-    queue.append(start)
-    
-    while queue:
-        node_u = queue.popleft()
-        print(node_u)
-        for node_v in graph[node_u]:
-            if node_v not in visited:
-                visited.add(node_v)
-                queue.append(node_v)
+class Solution:
+    def bfs(self, graph, u):
+        visited = set()                     # 使用 visited 标记访问过的节点
+        queue = collections.deque([])       # 使用 queue 存放临时节点
+        
+        visited.add(u)                      # 将起始节点 u 标记为已访问
+        queue.append(u)                     # 将起始节点 u 加入队列中
+        
+        while queue:                        # 队列不为空
+            u = queue.popleft()             # 取出队头节点 u
+            print(u)                        # 访问节点 u
+            for v in graph[u]:              # 遍历节点 u 的所有未访问邻接节点 v
+                if v not in visited:        # 节点 v 未被访问
+                    visited.add(v)          # 将节点 v 标记为已访问
+                    queue.append(v)         # 将节点 v 加入队列中
                 
 
 graph = {
-    "A": ["B", "C"],
-    "B": ["A", "C", "D"],
-    "C": ["A", "B", "D", "E"],
-    "D": ["B", "C", "E", "F"],
-    "E": ["C", "D"],
-    "F": ["D"]
+    "0": ["1", "2"],
+    "1": ["0", "2", "3"],
+    "2": ["0", "1", "3", "4"],
+    "3": ["1", "2", "4", "5"],
+    "4": ["2", "3"],
+    "5": ["3", "6"],
+    "6": []
 }
 
 # 基于队列实现的广度优先搜索
-bfs(graph, "A")
+Solution().bfs(graph, "0")
